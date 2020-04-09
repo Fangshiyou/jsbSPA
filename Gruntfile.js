10b9b22ad32038327d97d3c51520a688e1cd3bb1
Fangshiyou <1244841562@qq.com>
2020-04-09 09:50:45 +0800

    jsb

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports=function(grunt){
+    grunt.initConfig({
+        htmlhint: {
+            options: {
+              htmlhintrc: '.htmlhintrc'
+            },
+            src: 'index.html'
+        },
+        csslint: {
+            options: {
+              csslintrc: '.csslintrc'
+            },
+            src:'./fontbox/font.css'
+        },
+
+        htmlmin:{
+            options:{
+                removeComments:true,
+                collapseWhitespace:true
+            },
+            files:{
+                src:'./index.html',
+                dest:'dist/index.html'
+            }
+        },
+        cssmin: {
+            'dist/css/font.css': 'fontbox/font.css'
+        },
+        uglify: {
+          release:{
+              files:{
+                 'dist/js/font.js': 'fontbox/font.js'   
+              }
+          }
+        },
+        concat: {
+            js: {
+              src:'./**/*.js',
+              dest: 'dist/bundle.js'
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.loadNpmTasks('grunt-htmlhint');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('list', ['concat']);
+    grunt.registerTask('default', ['htmlhint', 'csslint']);
+    grunt.registerTask('minify',['htmlmin','cssmin','uglify'])
+}
